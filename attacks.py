66234dc1377e4d639b595eedc653a784a30b1d69
GiilDe <gilad.deutch@gmail.com>
2023-04-25 07:08:24 +0000

    first commit

---
+++ attacks.py
@@ -0,0 +1,210 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class PGDAttack:
+    """
+    White-box L_inf PGD attack using the cross-entropy loss
+    """
+
+    def __init__(self, model, eps=8/255., n=50, alpha=1/255.,
+                 rand_init=True, early_stop=True):
+        """
+        Parameters:
+        - model: model to attack
+        - eps: attack's maximum norm
+        - n: max # attack iterations
+        - alpha: step size at each iteration
+        - rand_init: a flag denoting whether to randomly initialize
+              adversarial samples in the range [x-eps, x+eps]
+        - early_stop: a flag denoting whether to stop perturbing a
+              sample once the attack goal is met. If the goal is met
+              for all samples in the batch, then the attack returns
+              early, before completing all the iterations.
+        """
+        self.model = model
+        self.n = n
+        self.alpha = alpha
+        self.eps = eps
+        self.rand_init = rand_init
+        self.early_stop = early_stop
+        self.loss_func = nn.CrossEntropyLoss(reduction='none')
+
+    def execute(self, x, y, targeted=False):
+        """
+        Executes the attack on a batch of samples x. y contains the true labels 
+        in case of untargeted attacks, and the target labels in case of targeted 
+        attacks. The method returns the adversarially perturbed samples, which
+        lie in the ranges [0, 1] and [x-eps, x+eps]. The attack optionally 
+        performs random initialization and early stopping, depending on the 
+        self.rand_init and self.early_stop flags.
+        """
+        mask = torch.ones(len(x))
+        x_original = torch.clone(x)
+        if self.rand_init:
+            x = x + torch.FloatTensor(x.shape).uniform_(-self.eps, self.eps)
+            x = torch.clip(x, 0, 1)
+        for _ in range(self.n):
+            x.requires_grad = True
+            x.grad = None
+            outputs = self.model(x)
+            loss = self.loss_func(outputs, y)
+            if targeted:
+                loss = -loss
+            loss.sum().backward()
+            x.requires_grad = False
+            x = x + self.alpha * \
+                np.sign(
+                    torch.mul(
+                        x.grad, mask.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)
+                    )
+                )
+            x = torch.clip(x, x_original - self.eps, x_original + self.eps)
+            x = torch.clip(x, 0, 1)
+            if self.early_stop:
+                outputs = outputs.argmax(dim=1)
+                mask = outputs == y if targeted else outputs != y
+                if torch.sum(mask) == len(y):
+                    return x
+                mask = ~mask
+
+        return x
+
+
+class NESBBoxPGDAttack:
+    """
+    Query-based black-box L_inf PGD attack using the cross-entropy loss, 
+    where gradients are estimated using Natural Evolutionary Strategies 
+    (NES).
+    """
+
+    def __init__(self, model, eps=8/255., n=50, alpha=1/255., momentum=0.,
+                 k=200, sigma=1/255., rand_init=True, early_stop=True):
+        """
+        Parameters:
+        - model: model to attack
+        - eps: attack's maximum norm
+        - n: max # attack iterations
+        - alpha: PGD's step size at each iteration
+        - momentum: a value in [0., 1.) controlling the "weight" of
+              historical gradients estimating gradients at each iteration
+        - k: the model is queries 2*k times at each iteration via 
+              antithetic sampling to approximate the gradients
+        - sigma: the std of the Gaussian noise used for querying
+        - rand_init: a flag denoting whether to randomly initialize
+              adversarial samples in the range [x-eps, x+eps]
+        - early_stop: a flag denoting whether to stop perturbing a 
+              sample once the attack goal is met. If the goal is met
+              for all samples in the batch, then the attack returns
+              early, before completing all the iterations.
+        """
+        self.model = model
+        self.eps = eps
+        self.n = n
+        self.alpha = alpha
+        self.momentum = momentum
+        self.k = k
+        self.sigma = sigma
+        self.rand_init = rand_init
+        self.early_stop = early_stop
+        self.loss_func = nn.CrossEntropyLoss(reduction='none')
+
+    def execute(self, x, y, targeted=False):
+        """
+        Executes the attack on a batch of samples x. y contains the true labels 
+        in case of untargeted attacks, and the target labels in case of targeted 
+        attacks. The method returns:
+        1- The adversarially perturbed samples, which lie in the ranges [0, 1] 
+        and [x-eps, x+eps].
+        2- A vector with dimensionality len(x) containing the number of queries for
+        each sample in x.
+        """
+        def estimate_grad():
+            loss = torch.zeros(x.shape)
+            for _ in range(self.k):
+                noize = torch.normal(
+                    mean=0, std=self.sigma, size=x.shape, dtype=x.dtype, device=x.device)
+                noize_minus = -noize
+                loss += torch.mul(noize, self.loss_func(self.model(x + noize),
+                                  y).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1))
+                loss += torch.mul(noize_minus, self.loss_func(self.model(x + noize),
+                                  y).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1))
+
+            return loss/(2*self.k*self.sigma)
+
+        mask = torch.ones(len(x))
+        prev_gradient = torch.zeros_like(x)
+        x_original = torch.clone(x)
+        n_queries = [self.n]*len(x)
+        if self.rand_init:
+            x = x + torch.FloatTensor(x.shape).uniform_(-self.eps, self.eps)
+            x = torch.clip(x, 0, 1)
+        for i in range(self.n):
+            x.requires_grad = True
+            x.grad = None
+            outputs = self.model(x)
+            loss = self.loss_func(outputs, y)
+            if targeted:
+                loss = -loss
+            loss.sum().backward()
+            x.requires_grad = False
+            grad = self.momentum*prev_gradient + \
+                (1 - self.momentum)*estimate_grad()
+            x = x + self.alpha * \
+                np.sign(
+                    torch.mul(grad, mask.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)))
+            x = torch.clip(x, x_original - self.eps, x_original + self.eps)
+            x = torch.clip(x, 0, 1)
+            if self.early_stop:
+                outputs = outputs.argmax(dim=1)
+                mask = outputs == y if targeted else outputs != y
+                n_queries = [min(n_queries_sample, i) if mask_sample else n_queries_sample for n_queries_sample,
+                             mask_sample in zip(n_queries, mask)]
+                if torch.sum(mask) == len(y):
+                    return x
+                mask = ~mask
+            prev_gradient = grad
+
+        return x, n_queries
+
+
+class PGDEnsembleAttack:
+    """
+    White-box L_inf PGD attack against an ensemble of models using the 
+    cross-entropy loss
+    """
+
+    def __init__(self, models, eps=8/255., n=50, alpha=1/255.,
+                 rand_init=True, early_stop=True):
+        """
+        Parameters:
+        - models (a sequence): an ensemble of models to attack (i.e., the
+              attack aims to decrease their expected loss)
+        - eps: attack's maximum norm
+        - n: max # attack iterations
+        - alpha: PGD's step size at each iteration
+        - rand_init: a flag denoting whether to randomly initialize
+              adversarial samples in the range [x-eps, x+eps]
+        - early_stop: a flag denoting whether to stop perturbing a 
+              sample once the attack goal is met. If the goal is met
+              for all samples in the batch, then the attack returns
+              early, before completing all the iterations.
+        """
+        self.models = models
+        self.n = n
+        self.alpha = alpha
+        self.eps = eps
+        self.rand_init = rand_init
+        self.early_stop = early_stop
+        self.loss_func = nn.CrossEntropyLoss()
+
+    def execute(self, x, y, targeted=False):
+        """
+        Executes the attack on a batch of samples x. y contains the true labels 
+        in case of untargeted attacks, and the target labels in case of targeted 
+        attacks. The method returns the adversarially perturbed samples, which
+        lie in the ranges [0, 1] and [x-eps, x+eps].
+        """
+        pass  # FILL ME
---
+++ utils.py
@@ -0,0 +1,141 @@
+import gzip
+import struct
+from os import path
+import numpy as np
+import models
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+import itertools
+
+def load_pretrained_cnn(cnn_id, n_classes=4, models_dir='trained-models/'):
+    """
+    Loads one of the pre-trained CNNs that will be used throughout the HW
+    """
+    if not isinstance(cnn_id, int) or cnn_id<0 or cnn_id>2:
+        raise ValueError(f'Unknown cnn_id {id}')
+    model = eval(f'models.SimpleCNN{cnn_id}(n_classes=n_classes)')
+    fpath = path.join(models_dir, f'simple-cnn-{cnn_id}')
+    model.load_state_dict(torch.load(fpath))
+    return model
+
+class TMLDataset(Dataset):
+    """
+    Used to load the dataset used throughout the HW
+    """
+    def __init__(self, fpath='dataset.npz', transform=None):
+        with gzip.open(fpath, 'rb') as fin:
+            self.data = np.load(fin, allow_pickle=True)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        x, y = self.data[idx]
+        if self.transform:
+            x = self.transform(x)
+        return x, y
+
+def compute_accuracy(model, data_loader, device):
+    """
+    Evaluates and returns the (benign) accuracy of the model 
+    (a number in [0, 1]) on the labeled data returned by 
+    data_loader.
+    """
+    batches_num = len(data_loader)
+    batches_acc = 0
+    for x, y in data_loader:
+        x = x.to(device)
+        y = y.to(device)
+        y_hat = model(x)
+        y_hat = torch.argmax(y_hat, dim=1)
+        batches_acc += torch.sum(y_hat == y).float()/len(y)
+    return batches_acc/batches_num    
+
+def run_whitebox_attack(attack, data_loader, targeted, device, n_classes=4):
+    """
+    Runs the white-box attack on the labeled data returned by
+    data_loader. If targeted==True, runs targeted attacks, where
+    targets are selected at random (t=c_x+randint(1, n_classes)%n_classes).
+    Otherwise, runs untargeted attacks. 
+    The function returns:
+    1- Adversarially perturbed sampels (one per input sample).
+    2- True labels in case of untargeted attacks, and target labels in
+       case of targeted attacks.
+    """
+    perturbed_xs = []
+    labels = []
+    for x, y in data_loader:
+        x = x.to(device)
+        y = y.to(device)
+        if targeted:
+            t = (y+torch.randint(1, n_classes, size=(len(y),)))%n_classes
+        perturbed_x = attack.execute(x, t if targeted else y, targeted)
+        perturbed_xs.append(perturbed_x)
+        labels.append(t if targeted else y)
+    return torch.cat(perturbed_xs), torch.cat(labels)
+
+def run_blackbox_attack(attack, data_loader, targeted, device, n_classes=4):
+    """
+    Runs the black-box attack on the labeled data returned by
+    data_loader. If targeted==True, runs targeted attacks, where
+    targets are selected at random (t=(c_x+randint(1, n_classes))%n_classes).
+    Otherwise, runs untargeted attacks. 
+    The function returns:
+    1- Adversarially perturbed sampels (one per input sample).
+    2- True labels in case of untargeted attacks, and target labels in
+       case of targeted attacks.
+    3- The number of queries made to create each adversarial example.
+    """
+    perturbed_xs = []
+    labels = []
+    n_queries = []
+    for x, y in data_loader:
+        x = x.to(device)
+        y = y.to(device)
+        if targeted:
+            t = (y+torch.randint(1, n_classes, size=(len(y),)))%n_classes
+        perturbed_x, batch_n_queries = attack.execute(x, t if targeted else y, targeted)
+        perturbed_xs.append(perturbed_x)
+        labels.append(t if targeted else y)
+        n_queries.append(batch_n_queries)
+    return torch.cat(perturbed_xs), torch.cat(labels), itertools.chain.from_iterable(n_queries)
+
+def compute_attack_success(model, x_adv, y, batch_size, targeted, device):
+    """
+    Returns the success rate (a float in [0, 1]) of targeted/untargeted
+    attacks. y contains the true labels in case of untargeted attacks,
+    and the target labels in case of targeted attacks.
+    """
+    outputs = model(x_adv.to(device))
+    outputs = torch.argmax(outputs, dim=1)
+    if targeted:
+        return torch.sum(outputs == y)/y.shape[0]
+    elif (not targeted):
+        return torch.sum(outputs != y)/y.shape[0]
+
+def binary(num):
+    """
+    Given a float32, this function returns a string containing its
+    binary representation (in big-endian, where the string only
+    contains '0' and '1' characters).
+    """
+    pass # FILL ME
+
+def float32(binary):
+    """
+    This function inverts the "binary" function above. I.e., it converts 
+    binary representations of float32 numbers into float32 and returns the
+    result.
+    """
+    pass # FILL ME
+
+def random_bit_flip(w):
+    """
+    This functoin receives a weight in float32 format, picks a
+    random bit to flip in it, flips the bit, and returns:
+    1- The weight with the bit flipped
+    2- The index of the flipped bit in {0, 1, ..., 31}
+    """
+    pass # FILL ME
